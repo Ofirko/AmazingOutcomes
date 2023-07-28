d2ccc353856c9c3bf94a9642439322c3117a5cd5
Scott Fletcher <scott@amazing-outcomes.de>
2023-07-28 09:25:14 +0200

    add players, scoring, printing

--- tennis/src/App.test.tsx
+++ tennis/src/App.test.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import * as App from './App';
 
-test('Game object exists', ()=> {
-  expect(App.game).toBeDefined();
+test('Game object exists', () => {
+    expect(App.game).toBeDefined();
 });
 
-test('Players object exists', ()=> {
-  expect(App.players).toBeDefined();
+test('Players object exists', () => {
+    expect(App.players).toBeDefined();
 });
 
 
-test('Players object has 2 players', ()=> {
-  expect(App.players).toHaveProperty();
+test('Players object has 2 players', () => {
+    expect(App.players).toHaveProperty('player1');
+    expect(App.players).toHaveProperty('player2');
 });
+
+test('Players start with score 0', () => {
+    expect(App.players.player1).toHaveProperty('score', 0);
+    expect(App.players.player2).toHaveProperty('score', 0);
+});
+
+test('Game prints score as love-love', () => {
+    expect(App.game.getScore()).toBe('love-love');
+})
+
+test('Player 1 scores a point', () => {
+    App.game.scorePoint(App.players.player1);
+    expect(App.players.player1).toHaveProperty('score', 1);
+})
+
+test('Player 2 scores two point', () => {
+    App.game.scorePoint(App.players.player2);
+    App.game.scorePoint(App.players.player2);
+    expect(App.players.player2).toHaveProperty('score', 2);
+})
+
+test('Game prints score as 15-30', () => {
+    expect(App.game.getScore()).toBe('15-30');
+})
--- tennis/src/App.tsx
+++ tennis/src/App.tsx
@@ -2,5 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export const game = {};
-export const players = {};
+type Player = {
+    score: number
+}
+
+export const game = {
+    scorePoint: (player: Player) => {
+        player.score++;
+    },
+    getScore: () => {
+        return 'love-love';
+    }
+};
+
+export const players: {player1: Player, player2: Player} = {
+    player1: {
+        score: 0
+    },
+    player2: {
+        score: 0
+    }
+};
+
+
